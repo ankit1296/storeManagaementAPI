302183b48c9bd5cf1296443056dbe2ea0637d73e
apk1296 <karveankit40@gmail.com>
2020-02-15 13:47:47 +0530

    initial commit

---
+++ src/test/java/com/ap/storeManagement/StoreManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ap.storeManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoreManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
